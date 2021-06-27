cbd8d4f78b485b560e032a2e05ed06354ef7cb0b
zorzalerrante <eduardo.graells@gmail.com>
2021-06-27 10:18:17 +0200

    including a modified version of ark-twokenize in aves.features

---
+++ src/aves/features/twokenize/__init__.py
@@ -0,0 +1,5 @@
+from .ark_twokenize import tokenize
+
+# code included from https://github.com/myleott/ark-twokenize-py/blob/master/twokenize.py
+# under the GPL-3 License
+# modifications include updates to support current emoji and more types of whitespace

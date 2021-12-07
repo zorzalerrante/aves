706f724555b950cd119aa5667df461fc15123087
ZorzalErrante <eduardo.graells@gmail.com>
2021-12-07 10:13:54 -0300

    feat: weighting dataframe with logodds ratio with uninformative dirichlet prior

--- src/aves/features/utils.py
+++ src/aves/features/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import scattertext
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.preprocessing import minmax_scale, normalize, quantile_transform
 
@@ -48,3 +49,19 @@ def tfidf(df, norm="l1", smooth_idf=False):
 def weighted_mean(df, value_column, weighs_column):
     weighted_sum = (df[value_column] * df[weighs_column]).sum()
     return weighted_sum / df[weighs_column].sum()
+
+
+def logodds_ratio_with_uninformative_dirichlet_prior(df, alpha_w=0.0001):
+    scorer = scattertext.LogOddsRatioUninformativeDirichletPrior(alpha_w=alpha_w)
+
+    top_words = {}
+
+    frequencies = df.sum(axis=0)
+
+    for idx, row in df.iterrows():
+        positive = row
+        negative = frequencies - positive
+        group_scores = scorer.get_scores(positive, negative)
+        top_words[idx] = group_scores
+
+    return pd.DataFrame(top_words).T

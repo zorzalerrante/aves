354f9d8620403cac85e82c42242c02f0a70afcb0
ZorzalErrante <eduardo.graells@gmail.com>
2022-05-16 21:13:35 -0400

    feat: bubble cloud with split

--- src/aves/visualization/tables/bubbles.py
+++ src/aves/visualization/tables/bubbles.py
@@ -8,6 +8,27 @@ from matplotlib.collections import PatchCollection
 from matplotlib.colors import ListedColormap
 import seaborn as sns
 
+import matplotlib.patches as mpatches
+
+
+def arc_patch(center, radius, theta1, theta2, resolution=50, **kwargs):
+    theta = np.linspace(np.radians(theta1), np.radians(theta2), resolution)
+    points = np.vstack(
+        (radius * np.cos(theta) + center[0], radius * np.sin(theta) + center[1])
+    )
+    poly = mpatches.Polygon(points.T, closed=True, **kwargs)
+    return poly
+
+
+def build_dual_bubble(pos=[0, 0], left_percentage=50, radio=1):
+    percentage = left_percentage
+    angle = 90 - (90 / 50) * percentage
+
+    return [
+        arc_patch((pos[0], pos[1]), radio, 270 - angle, 90 + angle),
+        arc_patch((pos[0], pos[1]), radio, -90 - angle, 90 + angle),
+    ]
+
 
 def bubble_plot(
     ax,
@@ -24,7 +45,14 @@ def bubble_plot(
     max_label_size=64,
     starting_y_range=None,
     margin=2,
+    dual=False,
+    dual_left_color="cornflowerblue",
+    dual_right_color="hotpink",
+    fontname=None,
+    fontstyle=None,
+    random_state=1990,
 ):
+    np.random.seed(random_state)
     df = df.reset_index()
     space = pymunk.Space()
     space.gravity = (0, 0)
@@ -58,7 +86,9 @@ def bubble_plot(
     value_range = (min(radius), max(radius))
 
     def scale(value):
-        result = (value - value_range[0]) / (value_range[1] - value_range[0])
+        result = (value - value_range[0] * 0.75) / (
+            value_range[1] - value_range[0] * 0.75
+        )
         return result
 
     ax.set_aspect("equal")
@@ -66,6 +96,7 @@ def bubble_plot(
     cmap = ListedColormap(sns.color_palette(palette, n_colors=n_bins))
 
     collection = []
+    arcs = []
     values = []
     min_x = 0
     max_x = 0
@@ -74,9 +105,6 @@ def bubble_plot(
 
     for body, (idx, row) in zip(space.bodies, df.iterrows()):
         circle = next(iter(body.shapes))
-        c = Circle(np.array(body.position), circle.radius - margin)
-        collection.append(c)
-        values.append(row[position_column])
 
         body_x = body.position.x
         body_min_x = body_x - circle.radius
@@ -98,6 +126,20 @@ def bubble_plot(
         if body_max_x > max_x:
             max_x = body_max_x
 
+        if dual == True:
+            arcs.append(
+                build_dual_bubble(
+                    pos=list(body.position),
+                    radio=circle.radius,
+                    # left_percentage=100-100*(body.position.x-min_x)/(max_x-min_x)
+                    # left_percentage=row.M*100
+                    left_percentage=100 - 100 * (row[position_column] + 1) / 2,
+                )
+            )
+        else:
+            c = Circle(np.array(body.position), circle.radius - margin)
+            collection.append(c)
+            values.append(row[position_column])
         if label_column is not None:
             label_size = int(scale(radius[idx]) * max_label_size)
             if label_size < min_label_size:
@@ -108,14 +150,31 @@ def bubble_plot(
                 ha="center",
                 va="center",
                 fontsize=label_size,
+                fontname=fontname,
+                fontstyle=fontstyle,
             )
 
-    collection = PatchCollection(collection, color="pink", edgecolor="none", cmap=cmap)
-    collection.set_array(np.array(values))
-    ax.add_collection(collection)
+    if dual == True:
+        split_collection_l = PatchCollection(
+            (a[0] for a in arcs), facecolors=dual_left_color, edgecolor="none"
+        )
+        ax.add_collection(split_collection_l)
+        split_collection_r = PatchCollection(
+            (a[1] for a in arcs), facecolors=dual_right_color, edgecolor="none"
+        )
+        ax.add_collection(split_collection_r)
+        collection = None
+    else:
+        collection = PatchCollection(
+            collection, color="pink", edgecolor="none", cmap=cmap
+        )
+        collection.set_array(np.array(values))
+        ax.add_collection(collection)
+        split_collection_l = None
+        split_collection_r = None
 
     ax.set_aspect("equal")
     ax.set_xlim([min_x, max_x])
     ax.set_ylim([min_y, max_y])
 
-    return space, collection
+    return space, collection, split_collection_l, split_collection_r

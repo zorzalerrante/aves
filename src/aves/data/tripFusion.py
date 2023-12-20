863fdb8c866c0068f37d8dc5007374908c148300
NatPupitre <natalia.meza.m@ug.uchile.cl>
2023-12-20 16:12:34 -0300

    función para definir atributos año, dia de la semana y hora

--- src/aves/data/tripFusion.py
+++ src/aves/data/tripFusion.py
@@ -90,3 +90,22 @@ def read_zones(data_path):
 Para nuestro objetivo
 zones = read_zones(file_path)
 """
+
+def year_day_hour(df):
+
+    semana2018 = semana2018.assign(
+        hour=lambda x: x["fecha"].dt.hour,
+        dayofweek=lambda x: x["fecha"].dt.dayofweek,
+        year = lambda x: x["fecha"].dt.year,
+        tiempo=1
+    )
+
+"""
+En nuestro caso hacemos
+semana2018 = semana2018.reset_index(drop=True)
+semana2019 = semana2019.reset_index(drop=True)
+
+ViajesTotales = dd.concat([semana2018, semana2019], axis=0)
+
+year_day_hour(ViajesTotales)
+"""

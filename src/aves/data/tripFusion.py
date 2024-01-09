9b001ffd90b6094eed3ddab733f136d53c94bb12
Natalia Meza <102987942+NatPupitre@users.noreply.github.com>
2024-01-09 16:54:05 -0300

    Update read_zones

--- src/aves/data/tripFusion.py
+++ src/aves/data/tripFusion.py
@@ -17,54 +17,6 @@ DATA_PATH = Path("/ruta a la carpeta")
 SCOOTERS_PATH = Path("/ruta a la carpeta")
 ADATRAP_PATH = Path("/ruta a la carpeta")
 
-#Recibe la fecha desde la cual se quiere leer los viajes (start_date) hasta (end_date)
-def read_date(date, data_path):
-    return dd.read_parquet(data_path/ f"{date}")
-
-def read_concat_date(start_date, end_date, data_path, output_name):
-    """
-    Recibe la fecha desde la cual se quiere leer los viajes (start_date) hasta (end_date),
-    los concatena y los guarda en un archivo parquet para luego ser leído directamente.
-
-    Parameters
-    ------------
-    start_date : string
-        Fecha desde la cual se comenzarán a leer los días.
-    end_date: string
-        Fecha hasta la cual se comenzarán a leer los días.
-    data_path: Path
-        Ruta de los archivos a leer y donde se guardara el nuevo arhivo concatenado.
-    output_name: string
-        Nombre del archivo resultante con la concatenación.
-
-    Returns
-    -------
-    df_concatenado
-        Dataframe con todas las fechas con el atributo que lo especifica
-    
-    """
-    # Genera las fechas entre start_date y end_date
-    date_range = [datetime.strftime(d, "%Y-%m-%d") for d in pd.date_range(start=start_date, end=end_date)]
-
-    # Lee los dataframs y agrega una columna de fecha
-    dataframes = []
-    for fecha in date_range:
-        df = dd.read_parquet(data_path / fecha)
-        df['fecha'] = datetime.strptime(fecha, "%Y-%m-%d")
-        dataframes.append(df)
-
-    # Concatena los dataframes
-    df_concatenado = dd.concat(dataframes, axis=0)
-
-    # Escribe el dataframe en un archivo Parquet
-    df_concatenado.to_parquet(data_path / f"{output_name}.parquet")
-
-"""
-para la semana 2018 y 2019 correspondería:
-read_concat_date('2018-05-14', '2018-05-20', DATA_PATH, "semana2018")
-read_concat_date('2019-05-13', '2019-05-19', DATA_PATH, "semana2019")
-"""
-
 from pyproj import CRS
 
 def read_zones(data_path):

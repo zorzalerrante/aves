e4f617dc38cf3375f01aea106731105e86c49773
ZorzalErrante <916285+zorzalerrante@users.noreply.github.com>
2024-06-03 09:48:14 -0400

    clase de reducción de complejidad

--- src/aves/config.py
+++ src/aves/config.py
@@ -2,10 +2,20 @@ import matplotlib as mpl
 import seaborn as sns
 
 
-def setup_style(style="whitegrid", context="paper", dpi=192, font_family="Inter"):
+def setup_style(
+    style="whitegrid",
+    context="paper",
+    dpi=192,
+    font_family="Inter",
+    formatter_limits=True,
+):
     sns.set_style(style)
     sns.set_context(context)
     # esto configura la calidad de la imagen. dependerá de tu resolución. el valor por omisión es 80
     mpl.rcParams["figure.dpi"] = dpi
     # esto depende de las fuentes que tengas instaladas en el sistema.
-    mpl.rcParams["font.family"] = font_family
+    if font_family in mpl.font_manager.get_font_names():
+        mpl.rcParams["font.family"] = font_family
+
+    if formatter_limits:
+        mpl.rcParams["axes.formatter.limits"] = (-99, 99)

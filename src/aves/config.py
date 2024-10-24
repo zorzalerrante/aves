94a8fa067a7a3ea1dc19b83c1bb9cb6d8c0d30a9
ZorzalErrante <916285+zorzalerrante@users.noreply.github.com>
2024-10-23 23:54:21 -0300

    clase de reducción de complejidad

--- src/aves/config.py
+++ src/aves/config.py
@@ -8,9 +8,10 @@ def setup_style(
     dpi=192,
     font_family="Inter",
     formatter_limits=True,
+    font_scale=0.9,
 ):
     sns.set_style(style)
-    sns.set_context(context)
+    sns.set_context(context, font_scale=font_scale)
     # esto configura la calidad de la imagen. dependerá de tu resolución. el valor por omisión es 80
     mpl.rcParams["figure.dpi"] = dpi
     # esto depende de las fuentes que tengas instaladas en el sistema.
